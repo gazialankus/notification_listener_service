157834145a1a3f94dca8e9981879a86cacaff173
Alp Sahin <alp@sahin.club>
2024-03-18 16:38:35 +0300

    Added media stream to example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:notification_listener_service/notification_event.dart';
 import 'package:notification_listener_service/notification_listener_service.dart';
+import 'package:notification_listener_service/track_info.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   StreamSubscription<ServiceNotificationEvent>? _subscription;
+  StreamSubscription<TrackInfo>? _mediaSubscription;
   List<ServiceNotificationEvent> events = [];
+  List<TrackInfo> tracks = [];
 
   @override
   void initState() {
@@ -75,6 +78,20 @@ class _MyAppState extends State<MyApp> {
                     const SizedBox(height: 20.0),
                     TextButton(
                       onPressed: () {
+                        _mediaSubscription = NotificationListenerService
+                            .mediaStream
+                            .listen((event) {
+                          log("$event");
+                          setState(() {
+                            tracks.add(event);
+                          });
+                        });
+                      },
+                      child: const Text("Start Media Stream"),
+                    ),
+                    const SizedBox(height: 20.0),
+                    TextButton(
+                      onPressed: () {
                         _subscription?.cancel();
                       },
                       child: const Text("Stop Stream"),
@@ -85,56 +102,81 @@ class _MyAppState extends State<MyApp> {
               Expanded(
                 child: ListView.builder(
                   shrinkWrap: true,
-                  itemCount: events.length,
-                  itemBuilder: (_, index) => Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: ListTile(
-                      onTap: () async {
-                        try {
-                          await events[index]
-                              .sendReply("This is an auto response");
-                        } catch (e) {
-                          log(e.toString());
-                        }
-                      },
-                      trailing: events[index].hasRemoved!
-                          ? const Text(
-                              "Removed",
-                              style: TextStyle(color: Colors.red),
-                            )
-                          : const SizedBox.shrink(),
-                      leading: events[index].appIcon == null
-                          ? const SizedBox.shrink()
-                          : Image.memory(
-                              events[index].appIcon!,
-                              width: 35.0,
-                              height: 35.0,
-                            ),
-                      title: Text(events[index].title ?? "No title"),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            events[index].content ?? "no content",
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 8.0),
-                          events[index].canReply!
+                  itemCount: events.length + tracks.length,
+                  itemBuilder: (_, index) {
+                    if (index < events.length) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: ListTile(
+                          onTap: () async {
+                            try {
+                              await events[index]
+                                  .sendReply("This is an auto response");
+                            } catch (e) {
+                              log(e.toString());
+                            }
+                          },
+                          trailing: events[index].hasRemoved!
                               ? const Text(
-                                  "Replied with: This is an auto reply",
-                                  style: TextStyle(color: Colors.purple),
+                                  "Removed",
+                                  style: TextStyle(color: Colors.red),
                                 )
                               : const SizedBox.shrink(),
-                          events[index].largeIcon != null
-                              ? Image.memory(
-                                  events[index].largeIcon!,
-                                )
-                              : const SizedBox.shrink(),
-                        ],
+                          leading: events[index].appIcon == null
+                              ? const SizedBox.shrink()
+                              : Image.memory(
+                                  events[index].appIcon!,
+                                  width: 35.0,
+                                  height: 35.0,
+                                ),
+                          title: Text(events[index].title ?? "No title"),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                events[index].content ?? "no content",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 8.0),
+                              events[index].canReply!
+                                  ? const Text(
+                                      "Replied with: This is an auto reply",
+                                      style: TextStyle(color: Colors.purple),
+                                    )
+                                  : const SizedBox.shrink(),
+                              events[index].largeIcon != null
+                                  ? Image.memory(
+                                      events[index].largeIcon!,
+                                    )
+                                  : const SizedBox.shrink(),
+                            ],
+                          ),
+                          isThreeLine: true,
+                        ),
+                      );
+                    }
+                    return ListTile(
+                      leading: const Icon(Icons.music_note),
+                      title: Text(
+                          tracks[index - events.length].title ?? 'Unknown'),
+                      trailing: Builder(
+                        builder: (context) {
+                          final playingState =
+                              tracks[index - events.length].state;
+                          if (playingState == 0) {
+                            return const Icon(Icons.play_arrow);
+                          } else if (playingState == 1) {
+                            return const Icon(Icons.pause);
+                          } else if (playingState == 2) {
+                            return const Icon(Icons.stop);
+                          } else {
+                            return const Icon(Icons.question_mark);
+                          }
+                        },
                       ),
-                      isThreeLine: true,
-                    ),
-                  ),
+                    );
+                  },
                 ),
               )
             ],
